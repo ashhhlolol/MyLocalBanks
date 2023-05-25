f2f25dcd2e595efae5807c5c82378d8b38d69393
Ashley <22000765@myrp.edu.sg>
2023-05-25 19:48:31 +0800

    -

--- app/src/main/java/sg/edu/rp/c346/id22000765/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000765/mylocalbanks/MainActivity.java
@@ -1,8 +1,11 @@
 package sg.edu.rp.c346.id22000765.mylocalbanks;
 
+import static android.graphics.Color.BLACK;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -10,8 +13,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(tvDBS);
         registerForContextMenu(tvOCBC);
         registerForContextMenu(tvUOB);
+
     }
 
     @Override
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         menu.add(0, 0, 0, "Website");
         menu.add(0, 1, 1, "Contact the bank");
+        menu.add(0, 2, 2, "Toggle Favourite");
 
         if (v == tvDBS) {
             wordClicked = "DBS";
@@ -62,32 +69,57 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
                 startActivity(intent);
                 return true;
+            } else if (item.getItemId() == 2) {
+                if (tvDBS.getCurrentTextColor() == Color.RED) {
+                    tvDBS.setTextColor(BLACK); // Reset text color to its original color
+                } else {
+                    tvDBS.setTextColor(Color.RED); // Set text color to red
+                }
+                return true;
             }
+            return false;
         }
 
-            if (wordClicked.equalsIgnoreCase("ocbc")) {
-                if (item.getItemId() == 1) {
-                    Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 18003633333"));
-                    startActivity(intentCall);
-                    return true;
-                } else if (item.getItemId() == 0) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
-                    startActivity(intent);
-                    return true;
+        if (wordClicked.equalsIgnoreCase("ocbc")) {
+            if (item.getItemId() == 1) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 18003633333"));
+                startActivity(intentCall);
+                return true;
+            } else if (item.getItemId() == 0) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
+                startActivity(intent);
+                return true;
+            } else if (item.getItemId() == 2) {
+                if (tvOCBC.getCurrentTextColor() == Color.RED) {
+                    tvOCBC.setTextColor(BLACK); // Reset text color to its original color
+                } else {
+                    tvOCBC.setTextColor(Color.RED); // Set text color to red
                 }
+                return true;
             }
-            if (wordClicked.equalsIgnoreCase("uob")) {
-                if (item.getItemId() == 1) {
-                    Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 18002222121"));
-                    startActivity(intentCall);
-                    return true;
-                } else if (item.getItemId() == 0) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
-                    startActivity(intent);
-                    return true;
-                }
+            return false;
 
+        }
+        if (wordClicked.equalsIgnoreCase("uob")) {
+            if (item.getItemId() == 1) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 18002222121"));
+                startActivity(intentCall);
+                return true;
+            } else if (item.getItemId() == 0) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
+                startActivity(intent);
+                return true;
+            }else if (item.getItemId() == 2) {
+                if (tvUOB.getCurrentTextColor() == Color.RED) {
+                    tvUOB.setTextColor(BLACK); // Reset text color to its original color
+                } else {
+                    tvUOB.setTextColor(Color.RED); // Set text color to red
+                }
+                return true;
             }
+            return false;
+        }
+
 
         return false;
     }
@@ -117,5 +149,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
